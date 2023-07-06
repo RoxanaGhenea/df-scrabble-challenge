b4e36b8a0d2a68ba1c1ebdda3a3e2a145e6aba04
Your Name <roxana_ghenea24@yahoo.com>
2023-07-07 02:24:38 +0300

    Test 3 - passed

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -21,7 +21,7 @@ public class ScrabbleTest {
     }
 
     @Test
-    @Disabled
+//    @Disabled
     public void returnsScoreForShortWord1() {
         Scrabble scrabble = new Scrabble("a");
         assertEquals(scrabble.score(), 1);
