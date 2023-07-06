a3ca91056bcc0400d2403f64026a135f2bf8893f
Your Name <roxana_ghenea24@yahoo.com>
2023-07-07 02:01:02 +0300

    Test 2 - passed

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -12,11 +12,21 @@ public class Scrabble {
     }
 
     public int score() {
+        if (!validateNull()) {
+            return 0;
+        }
+        return computeValidScore();
+    }
+    public int computeValidScore() {
         int answer = 0;
         for (char c : wordToScore.toCharArray()) {
             answer += letterScores.get(c);
         }
         return answer;
+    }
+
+    public boolean validateNull() {
+        return this.wordToScore != null;
     }
 }
 
